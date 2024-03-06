379a8448f8e144ae1908163e7fe6b824a02971b2
Harsh <harshsheth9909@gmail.com>
2024-03-07 00:07:55 +0530

    react website for verified inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import FormComponent from 'webpack2';
+
 
 function App() {
-  return (
+  const handleSubmit = (formData) => {
+    console.log('Submitted Data:', formData);
+    // Add your submission logic here
+  };
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <FormComponent onSubmit={handleSubmit}></FormComponent>
     </div>
   );
 }
